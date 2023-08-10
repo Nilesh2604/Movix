60ab733f2bbe2c6dd58b5c8c6a82a73ea3e199fb
Nilesh <sahaynilesh1@gmail.com>
2023-08-10 19:28:28 +0530

    changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./style.scss"
 import HeroBanner from './heroBanner/HeroBanner'
-import Trending from './trending/trending'
-import Popular from './popular/popular'
+import Trending from './trending/Trending'
+import Popular from './popular/Popular'
 import TopRated from './topRated/TopRated'
 const Home = () => {
   return (
